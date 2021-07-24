3ea51b464a5f99000304af8f21fe1a36442d5df8
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-07-24 14:47:58 +0700

    delete all

--- src/LayOut/TrendingPage/index.js
+++ src/LayOut/TrendingPage/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import Trending from '../../components/Trending'
 
 const TrendingPage = () => {
+    const  getData = async () => {
+        let data = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key=0869793797f938ae11efc2397000a89e')
+        let result =  data.json()
+        console.log(result)
+
+    }
+    getData()
     return (
         
         <div>

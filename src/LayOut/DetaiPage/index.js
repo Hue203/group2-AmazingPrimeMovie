eab64e1fbd691b3f1e7095521849de8b028dabcc
Tina <nguyennga.hcm@gmail.com>
2021-07-24 13:08:24 +0700

    insert the html link in index.html and start the detail page

--- src/LayOut/DetaiPage/index.js
+++ src/LayOut/DetaiPage/index.js
@@ -1,10 +1,37 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap";
+import "./Style.css";
 
 const DetaiPage = () => {
   return (
-    <>
-      <h1>this is my detail page</h1>
-    </>
+    <div className="all box1 ">
+      haha
+      <div className="_2Ke7Sf test">
+        haha
+        <div className=".avu-retail-page .fVWsBr.fVWsBr.fVWsBr test">
+          haha
+          <div className="_3KHiTg _2Ke7Sf test">
+            <img
+              id="atf-full"
+              class="Zk8aEm _2ZnCkI"
+              src="https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX1080_.jpg"
+              srcset="https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX360_.jpg 360w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX540_.jpg 540w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX720_.jpg 720w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX1080_.jpg 1080w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX1440_.jpg 1440w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX1620_.jpg 1620w, https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/NBCU-89Z24-Full-Image_GalleryBackground-en-US-1610386596939._SX2160_.jpg 2160w"
+              sizes="(max-width: 28em) 360px, (max-width: 55em) 720px, (min-width: 55em) 1080px, 1080px"
+              alt=""
+              onload="document.body.classList.add('atfimgloaded')"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 

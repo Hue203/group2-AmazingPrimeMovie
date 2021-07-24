c82dc60bcd181ff52c31e990a1ff6cc1acfccbc0
Hue203 <huenguyenmt.vn@gmail.com>
2021-07-25 00:20:01 +0700

    merger Tina ver5

--- src/LayOut/MovieDetaiPage/index.js
+++ src/LayOut/MovieDetaiPage/index.js
@@ -13,74 +13,79 @@ import "./Style.css";
 const DetaiPage = () => {
   return (
     <>
-      <Card className="_2Ke7Sf _3Lykuc">
+      <Card
+        className="_2Ke7Sf  picture-background _3Lykuc "
+        style={{
+          backgroundImage:
+            "linear-gradient(to right, #000 40%, transparent 58%), url(https://www.filmmattersmagazine.com/wp-content/uploads/2017/10/Shabani-1.jpg)",
+        }}
+      >
         <Card.Body className=" box1">
-          <div className="flex">
-            <div>
-              <Card.Title className="test">Special title treatment</Card.Title>
+          <Col xs={12} md={8}>
+            <div className="flex">
               <div>
-                <div className="test">stars, ranking</div>
-                <Row>
-                  <Col xs={8} md={6} className="test">
-                    <Row className="test">
-                      <Col xs={12} md={2} className="test">
-                        <Button variant="primary" className="button-color">
-                          Rent HD 3$
-                        </Button>
-                      </Col>
-                      <Col xs={12} md={2} className="test">
-                        <Button variant="primary " className="button-color">
-                          Rent HD 3$
-                        </Button>
-                      </Col>
-                      <Col xs={12} md={4} className="test">
-                        <Button variant="primary " className="button-color">
-                          More purchase options
-                        </Button>
-                      </Col>
-                      <Col xs={12} md={2} className="test">
-                        <Button
-                          variant="primary "
-                          className="button-color circle"
-                        >
-                          |>
-                        </Button>
-                      </Col>
-                      <Col xs={12} md={2} className="test">
-                        <Button
-                          variant="primary "
-                          className="button-color circle"
-                        >
-                          +
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </div>
-              <Card.Text>
-                Rentals include 30 days to start watching this video and 48
-                hours to finish once started.
-              </Card.Text>
-              <Card.Text className="test">
-                When a mysterious 10-year-old boy turns up who claims to live in
-                the woods with a giant green dragon, a forest ranger and an
-                11-year-old girl set out to learn the truth about him. Directors
-                David Lowery
-              </Card.Text>
-              <div className="test">
-                <Card.Text>Director :</Card.Text>
-                <Card.Text>Director :</Card.Text>
-                <Card.Text>Director :</Card.Text>
+                <Card.Title className="test">
+                  Special title treatment
+                </Card.Title>
+                <div>
+                  <div className="test">stars, ranking</div>
+                  <Row>
+                    <Col xs={8} md={6} className="test">
+                      <Row className="test">
+                        <Col xs={12} md={2} className="test">
+                          <Button variant="primary" className="button-color">
+                            Rent HD 3$
+                          </Button>
+                        </Col>
+                        <Col xs={12} md={2} className="test">
+                          <Button variant="primary " className="button-color">
+                            Rent HD 3$
+                          </Button>
+                        </Col>
+                        <Col xs={12} md={4} className="test">
+                          <Button variant="primary " className="button-color">
+                            More purchase options
+                          </Button>
+                        </Col>
+                        <Col xs={12} md={2} className="test">
+                          <Button
+                            variant="primary "
+                            className="button-color circle"
+                          >
+                            |>
+                          </Button>
+                        </Col>
+                        <Col xs={12} md={2} className="test">
+                          <Button
+                            variant="primary "
+                            className="button-color circle"
+                          >
+                            +
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </div>
+                <Card.Text>
+                  Rentals include 30 days to start watching this video and 48
+                  hours to finish once started.
+                </Card.Text>
+                <Card.Text className="test">
+                  When a mysterious 10-year-old boy turns up who claims to live
+                  in the woods with a giant green dragon, a forest ranger and an
+                  11-year-old girl set out to learn the truth about him.
+                  Directors David Lowery
+                </Card.Text>
+                <div className="test">
+                  <Card.Text>Director :</Card.Text>
+                  <Card.Text>Director :</Card.Text>
+                  <Card.Text>Director :</Card.Text>
+                </div>
               </div>
             </div>
-            <Card.Img
-              className=" center"
-              variant="top"
-              src="https://i.pinimg.com/564x/0d/a5/bd/0da5bd82b3071a3c92fe6bd70cd53ec4.jpg"
-              style={{ width: "30vw" }}
-            />
-          </div>
+          </Col>
+
           <div className="flex">
             <Card.Text className="test">
               Rentals include 30 days to start watching this video and 48 hours

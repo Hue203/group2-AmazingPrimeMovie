1718797735fc648e25c1b766fb4f823dfb3cf421
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-07-24 17:06:39 +0700

    done trending carousel

--- src/LayOut/TrendingPage/index.js
+++ src/LayOut/TrendingPage/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react' 
 import Trending from '../../components/Trending'
+import { useState } from 'react'
+import { useEffect } from 'react'
+const baseUrl = process.env.REACT_APP_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const imgLink = process.env.REACT_APP_IMAGE;
 
 const TrendingPage = () => {
-    const  getData = async () => {
-        let data = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key=0869793797f938ae11efc2397000a89e')
-        let result =  data.json()
-        console.log(result)
-
-    }
-    getData()
+    const [trendingMovies, setTrendingMovies] = useState([])
+    useEffect(()=>{
+        const getData = async () => {
+            try {
+             let res = await fetch(`${baseUrl}/trending/all/week?api_key=${API_KEY}`)
+             let data = await res.json() 
+             console.log(data)
+             setTrendingMovies(data.results)
+         } catch (error) {
+             console.log(error)
+         }
+     }
+     getData()
+    },[])
     return (
         
         <div>
-           <Trending />
+           <Trending trendingMovies={trendingMovies} imgLink={imgLink} />
         </div>
     )
 }
--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,45 +1,30 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
+import './style.css'
 
-const Trending = () => {
+const Trending = ({trendingMovies, imgLink}) => {
     return (
         
         <>
+        <Container>
         <Carousel>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={500}>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+          {trendingMovies && trendingMovies.map((movie)=>
+           <Carousel.Item interval={30000}>
+          
+           <img
+             className="trendingMovieImg"
+             src={`${imgLink}${movie.backdrop_path}`}
+             alt={`${movie.title}`}
+           />
+           
+           <Carousel.Caption>
+             <h3>{movie.title}</h3>
+             <h5>Watch Now! On Amazing Prime</h5>
+           </Carousel.Caption>
+         </Carousel.Item>
+          )}
 </Carousel>
+     </Container>
         </>
     )
 }

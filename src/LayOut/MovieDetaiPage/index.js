48f00e34a29fe3518d988f0a2049f4a57d7676fc
Tina <nguyennga.hcm@gmail.com>
2021-07-25 17:31:12 +0700

    DetaiPage = DetailPage1(components )+ Relate(components). Em da chia detail page thanh 2 file nho trong components

--- src/LayOut/MovieDetaiPage/index.js
+++ src/LayOut/MovieDetaiPage/index.js
@@ -1,136 +1,37 @@
-import React from "react";
-import {
-  Button,
-  Card,
-  Col,
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Row,
-} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import "./Style.css";
-// import Relate from "./DetailComp/Relate";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import Relate from "../../components/DetailComp/Relate";
+import DetaiPage1 from "../../components/DetailPage1";
+
+const baseUrl = process.env.REACT_APP_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const imgLink = process.env.REACT_APP_IMAGE;
 
 const DetaiPage = () => {
+  const [singleMovieData, setSingleMovieData] = useState([]);
+  useEffect(() => {
+    const getMovie1 = async () => {
+      try {
+        let url = `https://api.themoviedb.org/3/movie/602223?api_key=7b86993b7f0770e398a87a2da0766218&language=en-US`;
+        let res = await fetch(url);
+        let data = await res.json();
+        setSingleMovieData(data);
+        console.log("phimmoi", singleMovieData);
+        // return singleMovieData;
+      } catch (error) {
+        console.log("erro", error.msg);
+      }
+    };
+    getMovie1();
+  }, [singleMovieData.id]);
+
   return (
     <>
-      <Card>
-        <div
-          className="_2Ke7Sf  picture-background _3Lykuc "
-          style={{
-            backgroundImage:
-              "linear-gradient(to right, #0f171e 50%, transparent 58%), linear-gradient(to top, #0f171e 20%, transparent 58%),url(https://www.filmmattersmagazine.com/wp-content/uploads/2017/10/Shabani-1.jpg)",
-          }}
-        >
-          <Card.Body className=" box1">
-            <Col xs={12} md={8}>
-              <div className="flex">
-                <div>
-                  <Card.Text className="test page-title">
-                    Special title treatment
-                  </Card.Text>
-                  <div>
-                    <div className="test">stars, ranking</div>
-                    <Row>
-                      <Col xs={8} md={8} className="test">
-                        <Row className="test">
-                          <Col xs={12} md={2} className="test">
-                            <Button
-                              variant="secondary"
-                              className="button-color"
-                            >
-                              Rent HD 3$
-                            </Button>
-                          </Col>
-                          <Col xs={12} md={2} className="test">
-                            <Button
-                              variant="secondary "
-                              className="button-color"
-                            >
-                              Rent HD 3$
-                            </Button>
-                          </Col>
-                          <Col xs={12} md={4} className="test">
-                            <Button
-                              variant="secondary "
-                              className="button-color"
-                            >
-                              More purchase options
-                            </Button>
-                          </Col>
-                          <Col xs={12} md={1} className="test center-circle">
-                            <Button
-                              variant="secondary "
-                              className="button-color circle"
-                            >
-                              |>
-                            </Button>
-                          </Col>
-                          <Col xs={12} md={1} className="test center-circle">
-                            <Button
-                              variant="secondary "
-                              className="button-color circle"
-                            >
-                              +
-                            </Button>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </div>
-                  <Card.Text>
-                    Rentals include 30 days to start watching this video and 48
-                    hours to finish once started.
-                  </Card.Text>
-                  <Card.Text className="test">
-                    When a mysterious 10-year-old boy turns up who claims to
-                    live in the woods with a giant green dragon, a forest ranger
-                    and an 11-year-old girl set out to learn the truth about
-                    him. Directors David Lowery
-                  </Card.Text>
-                  <div className="test">
-                    <Card.Text style={{ color: "#6d7f8b" }}>
-                      Director :
-                    </Card.Text>
-                    <Card.Text style={{ color: "#6d7f8b" }}>
-                      Director :
-                    </Card.Text>
-                    <Card.Text style={{ color: "#6d7f8b" }}>
-                      Director :
-                    </Card.Text>
-                  </div>
-                </div>
-              </div>
-            </Col>
+      <DetaiPage1 movies={singleMovieData} />
+      <br></br>
+      <br></br>
 
-            <div className="flex">
-              <Card.Text className="test">
-                Rentals include 30 days to start watching this video and 48
-                hours to finish once started.
-              </Card.Text>
-              <div>
-                <Row>
-                  <Col xs={12} md={4} className="test">
-                    <FontAwesomeIcon
-                      icon="fa-solid fa-share-nodes"
-                      className="white"
-                    />
-                  </Col>
-                  <Col xs={12} md={4} className="test">
-                    {/* <FontAwesomeIcon icon="coffee" className="white" /> */}
-                  </Col>
-                  <Col xs={12} md={4} className="test">
-                    <Button variant="secondary ">Feedback</Button>
-                  </Col>
-                </Row>
-              </div>
-            </div>
-          </Card.Body>
-        </div>
-      </Card>
-      {/* <Relate /> */}
+      <Relate className="margin" />
     </>
   );
 };

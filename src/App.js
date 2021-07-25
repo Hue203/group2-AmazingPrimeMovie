97407e82195098d56e1d6ded2b45b40005c74f95
Lê Đạt <lpddat@gmail.com>
2021-07-25 20:07:01 +0700

    add searchbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./LayOut/HomePage";
 import DetaiPage from "./LayOut/MovieDetaiPage";
 import PublicNavBar from "./components/PublicNavbar";
+import SearchBox from "./components/SearchBox/index";
 
 function App() {
+
+  const [query, setQuery] = useState("");
   return (
     <>
       <Router>
@@ -16,6 +20,7 @@ function App() {
           <Route path="/movie/:id" exact component={DetaiPage} />
         </Switch>
       </Router>
+       <SearchBox setQuery={setQuery} />
     </>
   );
 }

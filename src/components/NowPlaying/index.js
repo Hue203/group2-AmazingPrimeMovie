913bf47c651cb97b0534e6d8e4d3291f50d21443
Tina <nguyennga.hcm@gmail.com>
2021-07-25 17:40:07 +0700

    DetaiPage = DetailPage1(components )+ Relate(components). Em da chia detail page thanh 2 file nho trong components

--- src/components/NowPlaying/index.js
+++ src/components/NowPlaying/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "slick-carousel/slick/slick.css" ;
-import "slick-carousel/slick/slick-theme.css" ;
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import "./style.css";
